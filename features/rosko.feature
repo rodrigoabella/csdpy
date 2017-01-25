b932ab92dd9c9c6ebf617ed475380373a7a99d00
rodrigoabella <rodrigoabella@gmail.com>
2017-01-25 15:18:38 +0100

    agregado test bienvenida

--- features/rosko.feature
+++ features/rosko.feature
@@ -1,6 +1,6 @@
-Feature: Mars Rover moves on Mars
+Feature: Rosko
 
 
-Scenario: Rover land in Mars
-  Given Mars Rover landed
-  Then position should be 0,0
+Scenario: ingreso a la pantalla inicial
+  Given rosko iniciado
+  Then muestra titulo del juego pasapalabras
--- features/steps/rosko_step.py
+++ features/steps/rosko_step.py
@@ -3,11 +3,11 @@ from behave import *
 from splinter import Browser
 browser = Browser('flask',app=app)
 
-@given(u'Mars Rover landed')
+@given(u'rosko iniciado')
 def step_impl(context):
-	browser.visit('http://127.0.0.1:5000')
+    browser.visit('http://127.0.0.1:5000')
 
-@then(u'position should be {position}')
-def step_impl(context,position):
-	assert position in browser.html , position
+@then(u'muestra titulo del juego {mensaje}')
+def step_impl(context, mensaje):
+    assert mensaje.upper() in browser.html
 
